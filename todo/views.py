0b27f8619a1934f70b3a3cf796c2e9cad4415803
juhoi.kim <juhoi.kim@gmail.com>
2020-05-03 16:17:39 +0900

    Done Django-Mixin-Todo

---
+++ todo/views.py
@@ -0,0 +1,52 @@
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, ListView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
+
+# Create your views here.
+from todo.models import Todo
+
+
+class TodoVueOnlyTV(TemplateView):
+    template_name = 'todo/todo_vue_only.html'
+
+
+class TodoCV(CreateView):
+    model = Todo
+    fields = '__all__'
+    template_name = 'todo/todo_form.html'
+    success_url = reverse_lazy('todo:list')
+
+
+class TodoLV(ListView):
+    model = Todo
+    template_name = 'todo/todo_list.html'
+
+
+class TodoDelV(DeleteView):
+    model = Todo
+    template_name = 'todo/todo_confirm_delete.html'
+    success_url = reverse_lazy('todo:list')
+
+
+class TodoDelV2(DeleteView):
+    model = Todo
+    # template_name = 'todo/todo_confirm_delete.html'
+    success_url = reverse_lazy('todo:mixin')
+
+    def get(self, request, *args, **kwargs):
+        return self.delete(self, request, *args, **kwargs)
+
+
+class TodoMOMCV(MultipleObjectMixin, CreateView):
+    model = Todo
+    fields = '__all__'
+    template_name = 'todo/todo_form_list.html'
+    success_url = reverse_lazy('todo:mixin')
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        return super().post(request, *args, **kwargs)
